2718f9e31910b1e4d06f86999d88438a20dd4500
Shiyukine <benjbres@gmail.com>
2023-05-10 13:37:51 +0200

    changed port

--- app/src/main/java/com/shiyukine/scopeinteract/ConnectTcp.java
+++ app/src/main/java/com/shiyukine/scopeinteract/ConnectTcp.java
@@ -9,7 +9,7 @@ public class ConnectTcp extends AsyncTask<Object, Void, Boolean> {
     @Override
     protected Boolean doInBackground(Object... objs) {
         try {
-            Graph.socket = new Socket((Graph.isUsb ? "127.0.0.1" : (String)objs[0]), 30921);
+            Graph.socket = new Socket((Graph.isUsb ? "127.0.0.1" : (String)objs[0]), 30931);
             Graph.output = new PrintWriter(Graph.socket.getOutputStream());
             return true;
         } catch (Exception e) {
